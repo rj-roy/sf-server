7f72f39b56f2575da18f71fd7d6a3991b094efce
rj-roy <dpjdeveloper.me@gmainl.com>
2026-06-20 18:32:45 +0600

    feat(/api/startup/delete/)

--- sf.js
+++ sf.js
@@ -73,6 +73,12 @@ const run = async () => {
             res.send(result);
         });
 
+        app.delete('/api/startup/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await startupsCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
     } finally {
         // console.error('Error connecting to MongoDB:', error);
     };
